4bbee2b9ad810aa4401bd9646a088f9c4bf2ff50
Lily Jeong <superfreak.co.kr@gmail.com>
2024-11-06 10:49:25 +0900

    completed deployment setting - using reverse proxy (uWSGI & nginx), configured using environment variable.

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e4)fev@vrx0vs$+^k(9fq%xy#3g%$)e8&4i#1pke2^##((p0hb'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
